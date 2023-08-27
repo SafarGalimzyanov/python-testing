30549d89a6b496abd37102fca3a6678f82935b41
Safar Galimzyanov <safargalimzyanov@protonmail.com>
2023-08-27 18:40:03 +0600

    Step 8: replace if statements with assert

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,9 +1,6 @@
-#from exercises.capitalize import capitalize
 from capitalize import capitalize
 
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
+assert capitalize('hello') != 'Hello'
 
-if capitalize('') != '':
-    raise Exception('Функция работает неверно для пустой строки!')
+assert capitalize('') != ''
